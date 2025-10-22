a767cbe7cb9e55c4664687623fd207f5022b6af8
Kranthi Kumar Kaile <kranthikailewd@gmail.com>
2025-10-22 18:08:27 +0530

    Update script.js

---
+++ Toggle_Like_and_Unlike/script.js
@@ -0,0 +1,13 @@
+function onClickLikeButton() {
+    if (document.getElementById("puppyImage").src === "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/white-puppy-img.png") {
+        document.getElementById("puppyImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/white-puppy-liked-img.png";
+        document.getElementById("likeIcon").style.color = "#0967d2";
+        document.getElementById("likeButton").style.backgroundColor = "#0967d2";
+        document.getElementById("likeButton").style.color = "#cbd2d9";
+    } else if (document.getElementById("puppyImage").src === "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/white-puppy-liked-img.png") {
+        document.getElementById("puppyImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/white-puppy-img.png";
+        document.getElementById("likeIcon").style.color = "#cbd2d9";
+        document.getElementById("likeButton").style.backgroundColor = "#cbd2d9";
+        document.getElementById("likeButton").style.color = "#9aa5b1";
+    }
+}
